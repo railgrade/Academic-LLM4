9c0034fb06c0aa13e63e18b8db94ad6ff59d8f00
railgrade <114815802+railgrade@users.noreply.github.com>
2021-08-28 21:28:35 +0000

    Add Screenshot

--- Practice/API/claude-in-slack-api/slack.py
+++ Practice/API/claude-in-slack-api/slack.py
@@ -32,4 +32,43 @@ class SlackClient(AsyncWebClient):
             try:
                 resp = await self.conversations_history(channel=self.CHANNEL_ID, oldest=self.LAST_TS, limit=2)
                 print("r: ", resp)
-                msg = [msg["text"] for msg in res
+                msg = [msg["text"] for msg in resp["messages"] if msg["user"] == CLAUDE_BOT_ID]
+                if msg and not msg[-1].endswith("Typing…_"):
+                    return msg[-1]
+            except (SlackApiError, KeyError) as e:
+                print(f"Get reply error: {e}")
+
+            await asyncio.sleep(1)
+
+        raise Exception("Get replay timeout")
+
+    async def get_stream_reply(self):
+        l = 0
+        for _ in range(150):
+            try:
+                resp = await self.conversations_history(channel=self.CHANNEL_ID, oldest=self.LAST_TS, limit=2)
+                msg = [msg["text"] for msg in resp["messages"] if msg["user"] == CLAUDE_BOT_ID]
+                if msg:
+                    last_msg = msg[-1]
+                    more = False
+                    if msg[-1].endswith("Typing…_"):
+                        last_msg = str(msg[-1])[:-11] # remove typing…
+                        more = True
+                    diff = last_msg[l:]
+                    l = len(last_msg)
+                    yield diff
+                    if not more:
+                        break
+            except (SlackApiError, KeyError) as e:
+                print(f"Get reply error: {e}")
+
+            await asyncio.sleep(2)
+
+client = SlackClient(token=getenv("SLACK_USER_TOKEN"))
+
+if __name__ == '__main__':
+    async def server():
+        await client.open_channel()
+        while True:
+            prompt = input("You: ")
+         
