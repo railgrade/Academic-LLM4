8882347610d8689566ca6f8dff1bd3914f8de99b
railgrade <114815802+railgrade@users.noreply.github.com>
2021-09-19 06:09:06 +0000

    Bump to version 0.0.2

--- Practice/API/openai-chinese-mirror/demo.py
+++ Practice/API/openai-chinese-mirror/demo.py
@@ -4,4 +4,47 @@ from os import getenv
 
 from dotenv import load_dotenv
 # openai.log = "debug"
-load_d
+load_dotenv()
+
+# chat anywhere
+OPENAI_TOKEN = getenv("OPENAI_TOKEN")
+openai.api_key = OPENAI_TOKEN
+openai.api_base = "https://api.chatanywhere.com.cn/v1"
+
+# # close AI
+# CLOSEAI_TOKEN = getenv("CLOSEAI_TOKEN")
+# openai.api_key = CLOSEAI_TOKEN
+# openai.api_base = "https://api.closeai-proxy.xyz"
+
+
+
+# 非流式响应
+# completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world!"}])
+# print(completion.choices[0].message.content)
+
+def gpt_35_api_stream(messages: list):
+    """为提供的对话消息创建新的回答 (流式传输)
+
+    Args:
+        messages (list): 完整的对话消息
+        api_key (str): OpenAI API 密钥
+
+    Returns:
+        tuple: (results, error_desc)
+    """
+    try:
+        response = openai.ChatCompletion.create(
+            model='gpt-4', # gpt-3.5-turbo
+            messages=messages,
+            stream=True,
+        )
+        completion = {'role': '', 'content': ''}
+        for event in response:
+            if event['choices'][0]['finish_reason'] == 'stop':
+                # print(f'收到的完成数据: {completion}')
+                break
+            for delta_k, delta_v in event['choices'][0]['delta'].items():
+                # print(f'流响应数据: {delta_k} = {delta_v}')
+                completion[delta_k] += delta_v
+        messages.append(completion)  # 直接在传入参数 messages 中追加消息
+        return
