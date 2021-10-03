feac97e10b472efed8cb4b5897f820d1cd663c96
railgrade <114815802+railgrade@users.noreply.github.com>
2021-10-03 22:51:32 +0000

    edit todo UI simplification

--- Practice/Instruction_Tuning/bootstrap_instructions.py
+++ Practice/Instruction_Tuning/bootstrap_instructions.py
@@ -138,3 +138,82 @@ def generate_instruction_following_data(
     scorer = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=False)
 
     # now let's generate new instructions!
+    progress_bar = tqdm.tqdm(total=num_instructions_to_generate)
+    if machine_instruction_data:
+        progress_bar.update(len(machine_instruction_data))
+
+    # first we tokenize all the seed instructions and generated machine instructions
+    all_instructions = [d["instruction"] for d in seed_instruction_data] + [
+        d["instruction"] for d in machine_instruction_data
+    ]
+    all_instruction_tokens = [scorer._tokenizer.tokenize(inst) for inst in all_instructions]
+
+    while len(machine_instruction_data) < num_instructions_to_generate:
+        request_idx += 1
+
+        batch_inputs = []
+        for _ in range(request_batch_size):
+            # only sampling from the seed tasks
+            prompt_instructions = random.sample(seed_instruction_data, num_prompt_instructions)
+            prompt = encode_prompt(prompt_instructions)
+            batch_inputs.append(prompt)
+            # raise ValueError(prompt) # debug
+        decoding_args = utils.OpenAIDecodingArguments(
+            temperature=temperature,
+            n=1,
+            max_tokens=3072,  # hard-code to maximize the length. the requests will be automatically adjusted
+            top_p=top_p,
+            stop=["\n20", "20.", "20."],
+        )
+        request_start = time.time()
+        results = utils.openai_completion(
+            prompts=batch_inputs,
+            model_name=model_name,
+            batch_size=request_batch_size,
+            decoding_args=decoding_args,
+            logit_bias={"50256": -100},  # prevent the <|endoftext|> token from being generated
+        )
+        request_duration = time.time() - request_start
+
+        process_start = time.time()
+        instruction_data = []
+        for result in results:
+            new_instructions = post_process_gpt3_response(num_prompt_instructions, result)
+            instruction_data += new_instructions
+
+        total = len(instruction_data)
+        keep = 0
+        for instruction_data_entry in instruction_data:
+            # computing similarity with the pre-tokenzied instructions
+            new_instruction_tokens = scorer._tokenizer.tokenize(instruction_data_entry["instruction"])
+            with Pool(num_cpus) as p:
+                rouge_scores = p.map(
+                    partial(rouge_scorer._score_lcs, new_instruction_tokens),
+                    all_instruction_tokens,
+                )
+            rouge_scores = [score.fmeasure for score in rouge_scores]
+            most_similar_instructions = {
+                all_instructions[i]: rouge_scores[i] for i in np.argsort(rouge_scores)[-10:][::-1]
+            }
+            if max(rouge_scores) > 0.7:
+                continue
+            else:
+                keep += 1
+            instruction_data_entry["most_similar_instructions"] = most_similar_instructions
+            instruction_data_entry["avg_similarity_score"] = float(np.mean(rouge_scores))
+            machine_instruction_data.append(instruction_data_entry)
+            all_instructions.append(instruction_data_entry["instruction"])
+            all_instruction_tokens.append(new_instruction_tokens)
+            progress_bar.update(1)
+        process_duration = time.time() - process_start
+        print(f"Request {request_idx} took {request_duration:.2f}s, processing took {process_duration:.2f}s")
+        print(f"Generated {total} instructions, kept {keep} instructions")
+        utils.jdump(machine_instruction_data, os.path.join(output_dir, "regen.json"))
+
+
+def main(task, **kwargs):
+    globals()[task](**kwargs)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
